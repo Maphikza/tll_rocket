e993f234ce72a84dac1e4a56ed6da9ee108c6013
Maphikza <stapisi@protonmail.com>
2023-07-08 15:30:15 +0200

    Adding diesel databse and connecting it

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,29 @@
 #[macro_use]
 extern crate rocket;
+#[macro_use]
+extern crate rocket_sync_db_pools;
 use rocket::fs::{relative, FileServer};
 
+mod diesel_sqlite;
+pub mod schema;
+
 mod manual {
-    use rocket::{fs::NamedFile};
+    use rocket::fs::NamedFile;
     use std::path::{Path, PathBuf};
     #[get("/")]
     pub async fn index() -> Option<NamedFile> {
         let home_path = Path::new(super::relative!("static")).join("index.html");
         NamedFile::open(home_path).await.ok()
+    }
+    #[get("/fais")]
+    pub async fn fais() -> Option<NamedFile> {
+        let fais_path = Path::new(super::relative!("static")).join("fais.html");
+        NamedFile::open(fais_path).await.ok()
+    }
+    #[get("/register")]
+    pub async fn register() -> Option<NamedFile> {
+        let fais_path = Path::new(super::relative!("static")).join("register.html");
+        NamedFile::open(fais_path).await.ok()
     }
     #[get("/css/<path..>")]
     pub async fn css_file(path: PathBuf) -> Option<NamedFile> {
@@ -17,20 +32,11 @@ mod manual {
     }
 }
 
-mod legislation {
-    use rocket::{fs::NamedFile};
-    use std::path::{Path};
-    #[get("/fais")]
-    pub async fn fais() -> Option<NamedFile> {
-        let fais_path = Path::new(super::relative!("static/fais")).join("fais.html");
-        NamedFile::open(fais_path).await.ok()
-    }
-}
-
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![manual::index, legislation::fais])
+        .mount("/", routes![manual::index, manual::fais, manual::register])
         .mount("/", FileServer::from(relative!("static")))
         .mount("/css", routes![manual::css_file])
+        .attach(diesel_sqlite::stage())
 }
